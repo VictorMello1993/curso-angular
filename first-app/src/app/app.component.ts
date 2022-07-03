85b6bca811e2e1990a18e4034e4e68294a8c1221
Victor Mello <victor.mello@al.infnet.edu.br>
2022-07-03 10:44:31 -0300

    Conhecendo ciclos de vida do Angular

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 
   template: `
-    <app-title title="Boa noite"></app-title>
+    {{valor}}
+    <button (click)="adicionar()">Adicionar</button>
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent {
+// Conhecendo ciclo de vida do Angular
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
+  valor: number = 1
 
+  adicionar(): number{
+    return this.valor += 1
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit')
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked')
+  }
 }
