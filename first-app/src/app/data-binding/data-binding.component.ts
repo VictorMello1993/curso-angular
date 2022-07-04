c04fdb96b06f1ca3cf9ac0d5ca2ec025893ef088
Victor Mello <victor.mello@al.infnet.edu.br>
2022-07-04 13:06:02 -0300

    Conhecendo event binding

--- first-app/src/app/data-binding/data-binding.component.ts
+++ first-app/src/app/data-binding/data-binding.component.ts
@@ -13,8 +13,19 @@ export class DataBindingComponent implements OnInit {
   imgSrc: string = "https://pbs.twimg.com/profile_images/1378748001129234435/25b-uJqe_400x400.jpg"
   imgTitle: string = "Avatar"
 
+  position: {x: number, y: number} = {x: 0, y: 0}
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  alertaInfo(valor: MouseEvent){
+    console.log(valor)
+  }
+
+  mouseMoveEventTeste(event: MouseEvent){
+    this.position.x = event.offsetX
+    this.position.y = event.offsetY
   }
 }
