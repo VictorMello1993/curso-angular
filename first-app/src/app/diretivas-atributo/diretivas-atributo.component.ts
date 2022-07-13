0c5844dd8713702f766650cc857c37a9dc8e297e
Victor Mello <victor.mello@al.infnet.edu.br>
2022-07-13 08:27:27 -0300

    Conhecendo ngStyle e ngModel

--- first-app/src/app/diretivas-atributo/diretivas-atributo.component.ts
+++ first-app/src/app/diretivas-atributo/diretivas-atributo.component.ts
@@ -9,10 +9,31 @@ export class DiretivasAtributoComponent implements OnInit {
   constructor() { }
 
   valor: boolean = true;
+  heightPx: string = '20px';
+  backgroundColor: string = 'red';
+  textColor: string = '#FFF';
+
+  list: Array<{nome: string}> = []
+  nome: string = '';
 
   ngOnInit(): void {
     setInterval(() => {
       this.valor = !this.valor
+
+      if(this.heightPx === '20px'){
+        this.heightPx = '50px'
+        this.backgroundColor = 'blue'
+      }
+      else{
+        this.heightPx = '20px'
+        this.backgroundColor = 'red'
+      }
+
     }, 2000)
+  }
+
+  salvar(){
+    this.list.push({nome: this.nome})
+    this.nome = ''
   }
 }
