77a5cbc8c28da1bb3c49007b46335349b388cb26
Victor Mello <victor.mello@al.infnet.edu.br>
2022-07-20 22:59:40 -0300

    Implementação do conteúdo do todo list (parte 1)

--- todolist/src/app/shared/shared.module.ts
+++ todolist/src/app/shared/shared.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent
   ]
 })
 export class SharedModule { }
