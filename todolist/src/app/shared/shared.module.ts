787ca9a2c89fe85fb3a729e1d0fe84aaee7d2c30
Victor Mello <victor.mello@al.infnet.edu.br>
2022-07-21 23:04:15 -0300

    Implementação do campo de input de dados (parte 1)

--- todolist/src/app/shared/shared.module.ts
+++ todolist/src/app/shared/shared.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
+import { InputTextComponent } from './input-text/input-text.component';
+
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    InputTextComponent,
+
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    ContentComponent
+    ContentComponent,
+    InputTextComponent
   ]
 })
 export class SharedModule { }
