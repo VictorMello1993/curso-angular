fcfb35beed060ec6cb8a3cc99cd12bc478d05157
Victor Mello <victor.mello@al.infnet.edu.br>
2022-07-31 21:47:33 -0300

    Preparando o deploy no Heroku

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-app.use(express.static('./dist/'));
+const appName = 'todolist';
+const outputPath = `${__dirname}/dist/${appName}`;
+
+app.use(express.static(outputPath));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/todolist/'}),
+    res.sendFile(`${outputPath}/index.html`),
 );
 
 app.listen(process.env.PORT || 8080);
