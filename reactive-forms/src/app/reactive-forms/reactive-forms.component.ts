893c4c3bd9ef10f0788588fd945ae1b087fce8f2
Victor Mello <victor.mello@al.infnet.edu.br>
2022-08-13 17:09:29 -0300

    Conhecendo Reactive Forms (parte 1)

---
+++ reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.scss']
+})
+export class ReactiveFormsComponent implements OnInit {
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  // Instanciando um formulário e agrupando elementos internos
+  registerForm: FormGroup = this.formBuilder.group({
+    firstName: [''],
+    lastName: [''],
+  })
+
+  ngOnInit(): void {
+  }
+
+  // Exibindo o valor inserido do campo de input do formulário
+  submitForm(){
+    console.log(this.registerForm.value) //Exibe os valores preenchidos de todos os campos
+    console.log(this.registerForm.value.firstName)
+    console.log(this.registerForm.value.lastName)
+  }
+
+}
