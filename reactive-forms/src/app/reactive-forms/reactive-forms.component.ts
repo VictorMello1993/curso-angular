cadef0158f7685f101b9fd3f7187161e74d210f3
Victor Mello <victor.mello@al.infnet.edu.br>
2022-08-13 17:36:27 -0300

    Conhecendo os validators do Reactive Forms (touched, dirty, required)

--- reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
+++ reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -13,7 +13,7 @@ export class ReactiveFormsComponent implements OnInit {
 
   // Instanciando um formulário e agrupando elementos internos
   registerForm: FormGroup = this.formBuilder.group({
-    firstName: [''],
+    firstName: ['', Validators.required],
     lastName: [''],
   })
 
