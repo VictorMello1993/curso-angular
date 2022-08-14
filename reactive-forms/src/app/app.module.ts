02d50c1b3f3ac1932efbebeaf0031bd8052193bf
Victor Mello <victorsmello93@gmail.com>
2022-08-14 17:09:51 -0300

    Explorando mais sobre validators

--- reactive-forms/src/app/app.module.ts
+++ reactive-forms/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormsComponent
+    ReactiveFormsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    NgxMaskModule.forRoot({
+      dropSpecialCharacters: false
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
+++ reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -11,10 +11,15 @@ export class ReactiveFormsComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {
   }
 
+  REGEX_EMAIL: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"
+  REGEX_CPF: string = "([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"
+
   // Instanciando um formulário e agrupando elementos internos
   registerForm: FormGroup = this.formBuilder.group({
     firstName: ['', Validators.required],
-    lastName: [''],
+    lastName: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(5)]],
+    email: ['', [Validators.required, Validators.email, Validators.pattern(this.REGEX_EMAIL)]],
+    cpf: ['', [Validators.required, Validators.pattern(this.REGEX_CPF)]]
   })
 
   ngOnInit(): void {
@@ -22,10 +27,12 @@ export class ReactiveFormsComponent implements OnInit {
 
   // Exibindo o valor inserido do campo de input do formulário
   submitForm(){
-    if(this.registerForm.valid) //Só irá submeter quando o formulário inteiro for válido
-    console.log(this.registerForm.value) //Exibe os valores preenchidos de todos os campos
-    console.log(this.registerForm.value.firstName)
-    console.log(this.registerForm.value.lastName)
+    //Só irá submeter quando o formulário inteiro for válido
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value) //Exibe os valores preenchidos de todos os campos
+      console.log(this.registerForm.value.firstName)
+      console.log(this.registerForm.value.lastName)
+    }
   }
 
 }
